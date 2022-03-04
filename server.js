ae7e031edfe5854311e292224a62c3878b548a65
kevin <kevin.morales@lumno.udg.mx>
2022-03-04 00:02:58 -0600

    Actualizacion para subir a heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist','Frontend_ConnectedSolutions'));
-res.sendFile(path.join(__dirname,'dist','Frontend_ConnectedSolutions','index.html'));
+app.use(express.static(path.join(__dirname, 'dist','frontend-connected-solutions'));
+res.sendFile(path.join(__dirname,'dist','frontend-connected-solutions','index.html'));
 app.listen(process.env.PORT || 8080);
